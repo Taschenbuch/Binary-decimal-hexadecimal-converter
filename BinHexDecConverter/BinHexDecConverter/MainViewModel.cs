bb644186b99ffc6158625b1dd73361b3d8e1170c
Taschenbuch <alex.lackner@web.de>
2020-02-21 22:47:47 +0100

    expander with bit position (view model, not working)

--- BinHexDecConverter/BinHexDecConverter/MainViewModel.cs
+++ BinHexDecConverter/BinHexDecConverter/MainViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
+using BinHexDecConverter.NumberConverters;
 
 namespace BinHexDecConverter
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private DecBinHexRowViewModel _selectedDecBinHexRowValue;
+
         public MainViewModel()
         {
-            DeleteRowCommand          = new RelayCommand(DeleteRow);
-            AddEmptyRowCommand        = new RelayCommand(AddEmptyRow);
+            DeleteRowCommand                      = new RelayCommand(DeleteRow);
+            AddEmptyRowCommand                    = new RelayCommand(AddEmptyRow);
+            SetNibblesInBitPositionSectionCommand = new RelayCommand(SetNibblesInBitPositionSection);
         }
 
 
@@ -24,14 +29,75 @@ namespace BinHexDecConverter
             RowService.AddRowIfNoRowIsLeft(DecBinHexValues);
         }
 
+        private void SetNibblesInBitPositionSection()
+        {
+            var binaryString = SelectedDecBinHexRowValue.Binary;
+            if (binaryString.IsNullOrWhiteSpace())
+                return;
 
-        public ICommand DeleteRowCommand          { get; set; }
-        public ICommand AddEmptyRowCommand        { get; set; }
+            binaryString = SeparatorService.RemoveSeparatorBlanks(binaryString);
+            binaryString = SeparatorService.AddSeparatorBlanks(binaryString, 4);
+            var nibbles = binaryString.Split(" ")
+                                      .Reverse()
+                                      .ToList();
+
+            //Bit63To60 = nibbles[15];
+            //Bit59To56 = nibbles[14];
+            //Bit55To52 = nibbles[13];
+            //Bit51To48 = nibbles[12];
+            //Bit47To44 = nibbles[11];
+            //Bit43To40 = nibbles[10];
+            //Bit39To36 = nibbles[9];
+            //Bit35To32 = nibbles[8];
+            //Bit31To28 = nibbles[7];
+            //Bit27To24 = nibbles[6];
+            //Bit23To20 = nibbles[5];
+            //Bit19To16 = nibbles[4];
+            //Bit15To12 = nibbles[3];
+            //Bit11To8  = nibbles[2];
+            //Bit7To4   = nibbles[1];
+            Bit3To0   = nibbles[0];
+        }
+
+        public string Bit63To60 { get; set; }
+        public string Bit59To56 { get; set; }
+        public string Bit55To52 { get; set; }
+        public string Bit51To48 { get; set; }
+        public string Bit47To44 { get; set; }
+        public string Bit43To40 { get; set; }
+        public string Bit39To36 { get; set; }
+        public string Bit35To32 { get; set; }
+        public string Bit31To28 { get; set; }
+        public string Bit27To24 { get; set; }
+        public string Bit23To20 { get; set; }
+        public string Bit19To16 { get; set; }
+        public string Bit15To12 { get; set; }
+        public string Bit11To8  { get; set; }
+        public string Bit7To4   { get; set; }
+        public string Bit3To0   { get; set; }
+
+
+        public ICommand DeleteRowCommand                      { get; set; }
+        public ICommand AddEmptyRowCommand                    { get; set; }
+        public ICommand SetNibblesInBitPositionSectionCommand { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<DecBinHexRowViewModel> DecBinHexValues { get; set; } = new ObservableCollection<DecBinHexRowViewModel>() {new DecBinHexRowViewModel()};
+        public ObservableCollection<DecBinHexRowViewModel> DecBinHexValues { get; set; } = 
+            new ObservableCollection<DecBinHexRowViewModel>() {new DecBinHexRowViewModel()};
 
-        public DecBinHexRowViewModel SelectedDecBinHexRowValue { get; set; }
+        public DecBinHexRowViewModel SelectedDecBinHexRowValue
+        {
+            get => _selectedDecBinHexRowValue;
+            set
+            {
+                _selectedDecBinHexRowValue = value;
+                
+                if (value == null)
+                    return;
+
+                SetNibblesInBitPositionSection();
+            }
+        }
     }
 }
