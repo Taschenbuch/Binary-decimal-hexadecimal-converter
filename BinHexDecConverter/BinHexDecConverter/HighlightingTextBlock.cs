36d5f501e7772a1d947a81a4f21ebb71e1493d36
Taschenbuch <alex.lackner@web.de>
2020-02-28 00:45:19 +0100

    try out more..

--- BinHexDecConverter/BinHexDecConverter/HighlightingTextBlock.cs
+++ BinHexDecConverter/BinHexDecConverter/HighlightingTextBlock.cs
@@ -16,33 +16,9 @@ namespace BinHexDecConverter
             var dp = DependencyPropertyDescriptor.FromProperty(
                 TextBlock.TextProperty,
                 typeof(TextBlock));
-            dp.AddValueChanged(this, (sender, args) =>
+            dp.AddValueChanged(HighlightingTextBlock, (sender, args) =>
             {
-                var tb = sender as TextBlock;
-
-                if (tb.Text.Length == 0)
-                    return;
-
-                string textUpper  = tb.Text.ToUpper();
-                String toFind     = "1";
-                int    firstIndex = textUpper.IndexOf(toFind, StringComparison.OrdinalIgnoreCase);
-                String firstStr   = tb.Text.Substring(0, firstIndex);
-                String foundStr   = tb.Text.Substring(firstIndex, toFind.Length);
-                String endStr = tb.Text.Substring(firstIndex + toFind.Length,
-                    tb.Text.Length - (firstIndex + toFind.Length));
-
-                tb.Inlines.Clear();
-                var run = new Run();
-                run.Text = firstStr;
-                tb.Inlines.Add(run);
-                run            = new Run();
-                run.Background = Brushes.Yellow;
-                run.Text       = foundStr;
-                tb.Inlines.Add(run);
-                run      = new Run();
-                run.Text = endStr;
-
-                tb.Inlines.Add(run);
+                MessageBox.Show("text changed");
             });
         }
 
@@ -53,6 +29,8 @@ namespace BinHexDecConverter
 
             if (tb.Text.Length == 0)
                 return;
+
+
 
             string textUpper  = tb.Text.ToUpper();
             String toFind     = "1";
