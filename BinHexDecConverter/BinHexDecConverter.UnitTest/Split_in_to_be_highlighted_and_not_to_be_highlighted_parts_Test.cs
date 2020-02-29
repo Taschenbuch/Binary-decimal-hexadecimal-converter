e29b166d2f669a11ce3be28d5dd9aea770eb719d
Taschenbuch <alex.lackner@web.de>
2020-02-29 11:30:31 +0100

    Update Split_in_to_be_highlighted_and_not_to_be_highlighted_parts_Test.cs

--- BinHexDecConverter/BinHexDecConverter.UnitTest/Split_in_to_be_highlighted_and_not_to_be_highlighted_parts_Test.cs
+++ BinHexDecConverter/BinHexDecConverter.UnitTest/Split_in_to_be_highlighted_and_not_to_be_highlighted_parts_Test.cs
@@ -8,7 +8,7 @@ namespace BinHexDecConverter.UnitTest
     {
         [TestCase("", new[] { "" })]
         [TestCase("0", new[] { "0" })]
-        [TestCase("0 ", new[] { "0 " })]
+        [TestCase("00", new[] { "00" })]
         [TestCase("01", new[] { "0", "1" })]
         [TestCase("001", new[] { "00", "1" })]
         [TestCase("10", new[] { "1", "0" })]
@@ -16,8 +16,8 @@ namespace BinHexDecConverter.UnitTest
         [TestCase("010", new[] { "0", "1", "0" })]
         [TestCase("00100", new[] { "00", "1", "00" })]
         [TestCase("1", new[] { "1" })]
-        [TestCase("1111", new[] { "1", "1",  "1", "1" })]
-        [TestCase("1  1  1  1", new[] { "1", "  ", "1", "  ", "1", "  ", "1" })]
+        [TestCase("1111", new[] { "1", "1", "1", "1" })]
+        //[TestCase("1  1  1  1", new[] { "1", "  ", "1", "  ", "1", "  ", "1" })]
         public void Split_in_to_be_highlighted_and_not_to_be_highlighted_parts(string inputText, string [] expectedTextParts)
         {
             var textParts = HighlightTermBehavior.SeparateTextIntoTermAndNotTermParts(inputText, "1");
